06276d7cb7da51faa30776392ed79635cd1f1d3c
truongan106 <truongan106@gmail.com>
2017-04-07 14:48:18 +0700

    Update all menu

---
+++ menu.php
@@ -0,0 +1,69 @@
+<?php
+	$topMenu = '<nav class="navbar navbar-default">
+          <div class="container-fluid">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+            
+
+
+              
+              <a class="navbar-brand" href="#">Name of Ur App</a>
+
+              <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+              </button>
+            </div>
+
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+              <ul class="nav navbar-nav">
+                <li class="active"><a href="../1412002/index.html">Home <span class="sr-only">(current)</span></a></li>
+                <li><a href="../1412002/albums.html">Albums</a></li>
+                <li><a href="../1412002/blog.html">Blogs</a></li>
+                <li><a href="../1412002/about.html">Abouts</a></li>
+              </ul>
+              
+              
+            </div><!-- /.navbar-collapse -->
+          </div><!-- /.container-fluid -->
+        </nav>';
+        $leftMenu = '<div class="side-menu">
+    
+    <nav class="navbar navbar-default" role="navigation">
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <button type="button" class="navbar-toggle">
+
+                <span class="glyphicon glyphicon-menu-right"></span>
+                
+            </button>
+    
+
+    <!-- Main Menu -->
+    <div class="side-menu-container">
+        <ul class="nav navbar-nav">
+
+            <li class="active"><a href="#"><span class="glyphicon glyphicon-home"></span> Home</a></li>
+            <li ><a href="#"><span class="glyphicon glyphicon-picture"></span> Albums</a></li>
+            <li><a href="#"><span class="glyphicon glyphicon-pencil"></span> Blogs</a></li>
+            
+            
+            <li><a href="#"><span class="glyphicon glyphicon-user"></span> About</a></li>
+
+        </ul>
+    </div><!-- /.navbar-collapse -->';
+    $footer = '<nav class="navbar navbar-default navbar-fixed-bottom">
+          <div class="container-fluid">
+            
+            <p class="alignCenter">Copyright 2017 © lab01-1412002-UDPT</p> 
+          </div><!-- /.container-fluid -->
+        </nav>';
+?>
+
+
+
+
+
+
